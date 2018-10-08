ebb3f76701500f768c00afa155d290fbd5e74997
ken huang <ken.huang@eten.com.tw>
2018-10-08 11:34:39 +0800

    three commit

--- password-retry.py
+++ password-retry.py
@@ -1,10 +1,12 @@
 i = 3
 password = 'a123456'
 while i > 0:
+	i = i - 1
 	pwd = input('please input password')
 	if pwd == password:
 		print('login success!')
 		break
 	else:
-		i = i - 1
-		print('you have', i,'chance!')
+		print('invalid password!', end = " ")
+		if i > 0:
+			print('You have', i, 'chance')
